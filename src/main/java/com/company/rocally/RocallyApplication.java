d63fbf8aff32b207e7eb20a41ab594b56556110e
KJS <nus0503@naver.com>
2024-05-02 12:29:11 +0900

    Initial commit

---
+++ src/main/java/com/company/rocally/RocallyApplication.java
@@ -0,0 +1,13 @@
+package com.company.rocally;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocallyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocallyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/rocally/RocallyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.rocally;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocallyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
