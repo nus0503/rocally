4c87f5e66f060c0f8824ab617eaf54e718f8d8c7
KJS <nus0503@naver.com>
2024-05-06 19:18:33 +0900

    엔티티 repository

---
+++ src/main/java/com/company/rocally/domain/travel/TravelRepository.java
@@ -0,0 +1,8 @@
+package com.company.rocally.domain.travel;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface TravelRepository extends JpaRepository<Travel, Long> {
+
+}
