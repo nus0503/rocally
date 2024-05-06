3e46b5b289af214ad5e6bb465db6adee238e4406
KJS <nus0503@naver.com>
2024-05-06 19:17:42 +0900

    시큐리티 미적용 시큐리티 설정 후 제거

--- src/main/java/com/company/rocally/RocallyApplication.java
+++ src/main/java/com/company/rocally/RocallyApplication.java
@@ -2,8 +2,10 @@ package com.company.rocally;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class RocallyApplication {
 
 	public static void main(String[] args) {
