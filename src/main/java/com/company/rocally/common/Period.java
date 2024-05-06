a4e47a3cc865df76a1bcc51640c5a4ea71601d2b
KJS <nus0503@naver.com>
2024-05-06 19:13:28 +0900

    기간 포맷 공통 클래스

---
+++ src/main/java/com/company/rocally/common/Period.java
@@ -0,0 +1,8 @@
+package com.company.rocally.common;
+
+import java.time.format.DateTimeFormatter;
+
+public class Period {
+
+    public static DateTimeFormatter yyyyMMddHHmm = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+}
