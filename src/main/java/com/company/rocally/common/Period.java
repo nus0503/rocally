ea19766317f936f65dec70c20a013e13954e0d4d
KJS <nus0503@naver.com>
2024-05-07 20:07:45 +0900

    새로운 날짜 데이터 포맷형식 추가

--- src/main/java/com/company/rocally/common/Period.java
+++ src/main/java/com/company/rocally/common/Period.java
@@ -5,4 +5,6 @@ import java.time.format.DateTimeFormatter;
 public class Period {
 
     public static DateTimeFormatter yyyyMMddHHmm = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    public static DateTimeFormatter yyyyMMdd = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 }
