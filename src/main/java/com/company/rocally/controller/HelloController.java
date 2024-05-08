77dedc5eedf0a746cd2f1378fd319adfe422465a
KJS <nus0503@naver.com>
2024-05-08 18:49:28 +0900

    로그인한 유저의 세션을 이용해 로그인한 유저만 보이는 버튼

--- src/main/java/com/company/rocally/controller/HelloController.java
+++ src/main/java/com/company/rocally/controller/HelloController.java
@@ -1,13 +1,28 @@
 package com.company.rocally.controller;
 
+import com.company.rocally.config.auth.LoginUser;
+import com.company.rocally.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
+@RequiredArgsConstructor
 public class HelloController {
 
-    @GetMapping("/index")
-    public String hello() {
+    private final HttpSession httpSession;
+
+    @GetMapping("/")
+    public String hello(@LoginUser SessionUser user, Model model, HttpServletRequest request) {
+        if (user != null) {
+            String id = httpSession.getId();
+            HttpSession session = request.getSession();
+            model.addAttribute("user", user);
+        }
         return "index";
     }
 
@@ -15,4 +30,6 @@ public class HelloController {
     public String register() {
         return "travel-register";
     }
+
+
 }
