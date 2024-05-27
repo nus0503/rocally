8b444feba6ef1ad2383a42e3d545b274f0a47d6e
kjs <nus0503@naver.com>
2024-05-28 02:11:44 +0900

    정적 리소스를 처리하기 위해 핸들러 추가

--- src/main/java/com/company/rocally/config/WebConfig.java
+++ src/main/java/com/company/rocally/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.company.rocally.config.resolver.LoginUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -16,5 +17,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginUserArgumentResolver);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/attach/images/**")
+                .addResourceLocations("file:///Users/nus05/IdeaProject/uploadFile/");
     }
 }
