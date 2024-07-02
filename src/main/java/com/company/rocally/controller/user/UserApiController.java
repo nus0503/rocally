3af538dc6c839e164e20c1a69ba529b47d7c52b1
KJS <nus0503@naver.com>
2024-07-02 18:13:25 +0900

    기본 비밀번호와 유저 정보 수정 컨트롤러 메소드 추가

--- src/main/java/com/company/rocally/controller/user/UserApiController.java
+++ src/main/java/com/company/rocally/controller/user/UserApiController.java
@@ -1,17 +1,18 @@
 package com.company.rocally.controller.user;
 
 import com.company.rocally.common.validation.email.CheckEmailValidator;
+import com.company.rocally.common.validation.password.ExistingPasswordValidator;
+import com.company.rocally.controller.user.dto.PasswordChangeDto;
 import com.company.rocally.controller.user.dto.UserRegisterRequestDto;
+import com.company.rocally.controller.user.dto.UserUpdateRequestDto;
 import com.company.rocally.service.user.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -21,11 +22,18 @@ public class UserApiController {
 
     private final CheckEmailValidator checkEmailValidator;
 
+    private final ExistingPasswordValidator existingPasswordValidator;
+
     private final UserService userService;
 
     @InitBinder(value = "userRegisterRequestDto")
     public void validatorBinder(WebDataBinder binder) {
         binder.addValidators(checkEmailValidator);
+    }
+
+    @InitBinder(value = "passwordChangeDto")
+    public void passwordValidatorBinder(WebDataBinder binder) {
+        binder.addValidators(existingPasswordValidator);
     }
 
     @GetMapping("/signup")
@@ -52,4 +60,20 @@ public class UserApiController {
     public String dashboard() {
         return "dashboardIndex";
     }
+
+    @PostMapping("matchPassword")
+    public String matchPassword(PasswordChangeDto passwordChangeDto, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            model.addAttribute("passwordChangeDto", passwordChangeDto);
+            return "checkExistingPassword";
+        }
+        return "updateUser";
+    }
+
+    @PutMapping("/user")
+    @ResponseBody
+    public ResponseEntity<String> modify(@RequestBody UserUpdateRequestDto dto) {
+        return ResponseEntity.ok().build();
+    }
+
 }
---
+++ src/main/java/com/company/rocally/controller/user/dto/UserUpdateRequestDto.java
@@ -0,0 +1,4 @@
+package com.company.rocally.controller.user.dto;
+
+public class UserUpdateRequestDto {
+}
