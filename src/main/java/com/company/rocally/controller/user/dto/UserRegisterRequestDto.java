d48807fd5b1f06faf6706a5befcd34b836d6c7b1
kjs <nus0503@naver.com>
2024-07-03 13:34:58 +0900

    회원가입 dto 필드 추가

--- src/main/java/com/company/rocally/controller/user/dto/UserRegisterRequestDto.java
+++ src/main/java/com/company/rocally/controller/user/dto/UserRegisterRequestDto.java
@@ -4,6 +4,7 @@ import com.company.rocally.common.Period;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -17,6 +18,9 @@ public class UserRegisterRequestDto {
     @NotBlank(message = "이름을 입력하세요.")
     private String username;
 
+    private String yyyy;
+    private String mm;
+    private String dd;
     @NotNull(message = "생년월일을 입력해주세요.")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
@@ -24,7 +28,6 @@ public class UserRegisterRequestDto {
     @NotBlank(message = "이메일을 입력해주세요.")
     @Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$", message = "이메일 형식이 올바르지 않습니다.")
     private String email;
-
     @NotBlank(message = "비밀번호를 입력해주세요.")
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String password;
@@ -41,6 +44,17 @@ public class UserRegisterRequestDto {
             return birthDate.format(Period.yyyyMMdd);
         } else {
             return "";
+        }
+    }
+
+    @ModelAttribute
+    public void setBirthDate() {
+        if (yyyy != null && mm != null && dd != null) {
+            this.birthDate = LocalDate.of(
+                    Integer.parseInt(yyyy),
+                    Integer.parseInt(mm),
+                    Integer.parseInt(dd)
+            );
         }
     }
 }
