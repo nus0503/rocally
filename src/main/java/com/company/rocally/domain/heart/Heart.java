778930b331e858c61c370634c9de2a7305ca7b07
KJS <nus0503@naver.com>
2024-07-30 18:32:51 +0900

    좋아요 기능 프로토타입

---
+++ src/main/java/com/company/rocally/domain/heart/Heart.java
@@ -0,0 +1,28 @@
+package com.company.rocally.domain.heart;
+
+import com.company.rocally.domain.travel.Travel;
+import com.company.rocally.domain.user.User;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "hearts")
+@Entity
+public class Heart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "travel_id")
+    private Travel travel;
+}
---
+++ src/main/java/com/company/rocally/domain/heart/HeartRepository.java
@@ -0,0 +1,6 @@
+package com.company.rocally.domain.heart;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HeartRepository extends JpaRepository<Long, Heart> {
+}
