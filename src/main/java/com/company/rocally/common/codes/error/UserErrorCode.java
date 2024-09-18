6a5a2a05cc5afcdb60e09e304480f1631d09a896
kjs <nus0503@naver.com>
2024-09-19 03:33:27 +0900

    유저에러코드 추가생성

--- src/main/java/com/company/rocally/common/codes/error/UserErrorCode.java
+++ src/main/java/com/company/rocally/common/codes/error/UserErrorCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum UserErrorCode implements ErrorCode {
 
     INACTIVE_USER(HttpStatus.FORBIDDEN, "User is inactive"),
+    NOT_FOUND_INFORMATION(HttpStatus.NOT_FOUND, "등록된 정보가 없습니다.")
     ;
 
     private final HttpStatus httpStatus;
